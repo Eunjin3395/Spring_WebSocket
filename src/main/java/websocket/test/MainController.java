2e765872807914113c5745c86ae2d2889ff652ad
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-05-16 15:48:50 +0900

    STOMP 튜토리얼 코드

---
+++ src/main/java/websocket/test/MainController.java
@@ -0,0 +1,23 @@
+package websocket.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+import websocket.test.dto.JoinDTO;
+import websocket.test.dto.MessageDTO;
+
+@Controller
+@Slf4j
+public class MainController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public MessageDTO greeting(JoinDTO message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        log.info(message.getName());
+        MessageDTO value = new MessageDTO("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        log.info(value.getMessage());
+        return value ;
+    }
+}
---
+++ src/main/java/websocket/test/dto/MessageDTO.java
@@ -0,0 +1,12 @@
+package websocket.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDTO {
+    private String message;
+}
